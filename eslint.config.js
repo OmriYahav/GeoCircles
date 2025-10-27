5557a17a2f388caf46d30cf650a747f29e37ca99
Omri Yahav <122606148+OmriYahav@users.noreply.github.com>
2025-10-27 10:40:53 +0200

    Add restore control for map overlay

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,13 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      },
+      'import/resolver': {
+        typescript: {},
+      },
+    },
   },
 ]);
